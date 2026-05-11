655e440f97c76230deb5654b37102e5563bf3c21
Bernard Krehula <krehula.bernard456@gmail.com>
2026-05-11 20:47:58 +0200

    feat: add <homepage> comoponent

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,10 +1,16 @@
-import './index.css'
+import SearchBar from "../../components/ui/SearchBar";
+import "./index.css";
 
 const Homepage = () => {
-    return(
-        <div className="homepage">
-
-        </div>
-    )
-}
+  return (
+    <div className="homepage">
+      <div className="text-content">
+        <h1>The best pizza.</h1>
+        <h2>Straight out of the oven, straight to you.</h2>
+        <h4>👋 Welcome! Please start by telling us your name:</h4>
+      </div>
+      <SearchBar className="name-input" placeholder="Your full name" />
+    </div>
+  );
+};
 export default Homepage;
