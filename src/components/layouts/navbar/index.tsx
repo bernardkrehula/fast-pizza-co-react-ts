784c8677dbcf66e9edbca4a6d0b96b83bebc30c9
Bernard Krehula <krehula.bernard456@gmail.com>
2026-05-12 18:43:16 +0200

    feat: add reddirectToHomepage function to <NavBar> layout

--- src/components/layouts/navbar/index.tsx
+++ src/components/layouts/navbar/index.tsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import SearchBar from "../../ui/SearchBar";
 import "./index.css";
+import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+  const [inputValue, setInputValue] = useState<string>('');
+  const navigate = useNavigate();
+  
+  const reddirectToHomepage = () => {
+    navigate('/');
+  }
+
   return (
     <div className="navbar">
-      <h1>Fast React Pizza Co.</h1>
-      <SearchBar placeholder='Search order #'/>
+      <h1 onClick={reddirectToHomepage}>Fast React Pizza Co.</h1>
+      <SearchBar placeholder="Search order #" className="default-input" />
       <h2>Bernard</h2>
     </div>
   );
