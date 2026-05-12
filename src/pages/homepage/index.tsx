395d78b48958a19686fcbdc1997f7088a5087ce8
Bernard Krehula <krehula.bernard456@gmail.com>
2026-05-12 21:37:09 +0200

    feat: add activeBtn state, handle InputValue and reddirect to menu

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+import Btn from "../../components/ui/btn";
 import SearchBar from "../../components/ui/SearchBar";
 import "./index.css";
+import { useNavigate } from "react-router-dom";
 
 const Homepage = () => {
+  const [activeBtn, setActiveBtn] = useState(false);
+  const navigate = useNavigate();
+
+  const handleInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    if (value != "") return setActiveBtn(true);
+    return setActiveBtn(false);
+  };
+  const reddirectToMenu = () => navigate("/menu");
+
   return (
     <div className="homepage">
       <div className="text-content">
@@ -9,7 +22,18 @@ const Homepage = () => {
         <h2>Straight out of the oven, straight to you.</h2>
         <h4>👋 Welcome! Please start by telling us your name:</h4>
       </div>
-      <SearchBar className="name-input" placeholder="Your full name" />
+      <div className="name-form">
+        <SearchBar
+          onChange={handleInputValue}
+          className="name-input"
+          placeholder="Your full name"
+        />
+        {activeBtn && (
+          <Btn onClick={reddirectToMenu} type="button">
+            Start ordering
+          </Btn>
+        )}
+      </div>
     </div>
   );
 };
