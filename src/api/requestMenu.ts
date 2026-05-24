ce29510b1b1c90d7e0bfec95d5c9baf034c69638
Bernard Krehula <krehula.bernard456@gmail.com>
2026-05-24 09:53:09 +0200

    refactor: move request in try catch blok with handling errors

--- src/api/requestMenu.ts
+++ src/api/requestMenu.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 export const requestMenu = async () => {
   try {
@@ -6,5 +6,11 @@ export const requestMenu = async () => {
       "https://react-fast-pizza-api.onrender.com/api/menu",
     );
     return response.data;
-  } catch (error) {}
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      console.error(error.response?.data.message)
+      throw new Error(error.response?.data.message);
+    }
+    throw error;
+  }
 };
