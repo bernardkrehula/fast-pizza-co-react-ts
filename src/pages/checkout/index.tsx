6eebe1c2147c198dd43557ea39a54eb7249c5760
Bernard Krehula <krehula.bernard456@gmail.com>
2026-05-25 22:07:43 +0200

    feat: add try catch block in handleOrderValues to handle errors

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -1,5 +1,4 @@
-import { FormEvent } from "react";
-import { object, parse, string } from "valibot";
+import { type FormEvent, useState } from "react";
 import { reuqestOrder } from "../../api/requestOrder";
 import { useAppSelector } from "../../app/hooks";
 import Btn from "../../components/ui/btn";
@@ -8,25 +7,55 @@ import FormInput from "./FormInput";
 import "./index.css";
 import { useNavigate } from "react-router-dom";
 import { localErrorValidator } from "../../utils/localErrorValidator";
+import type { orderShema } from "../../utils/localErrorValidator";
+import * as v from "valibot";
+import { GenericError } from "../../utils/GenericError";
 
 const Checkout = () => {
   const cart = useAppSelector((state) => state.orders.orders);
   const navigate = useNavigate();
+  const [formError, setFormError] = useState<{customer: string; phone: string; address: string;}>({
+    customer: "",
+    phone: "",
+    address: "",
+  });
 
   const handleOrderValues = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const credentials = {
-      customer: formData.get("first_name"),
-      phone: formData.get("phone_number"),
-      address: formData.get("adress"),
-      priority: formData.has("priority"),
-    };
-    localErrorValidator(credentials);
-    console.log(credentials)
-    const order = { ...credentials, cart: cart, position: "" };
-    const { data: { id: orderId } } = await reuqestOrder(order);
-    reddirectToOrder(orderId);
+
+    try {
+      const formData = new FormData(e.currentTarget);
+      const credentials = {
+        customer: formData.get("first_name") as string,
+        phone: formData.get("phone_number") as string,
+        address: formData.get("adress") as string,
+        priority: formData.has("priority"),
+      };
+      localErrorValidator(credentials);
+      const order = { ...credentials, cart: cart, position: "" };
+      const {
+        data: { id: orderId },
+      } = await reuqestOrder(order);
+      reddirectToOrder(orderId);
+    } catch (error) {
+      if (error instanceof v.ValiError) {
+        const flatIssues = v.flatten<orderShema>(error.issues);
+        const formErrors: Partial<Record<v.IssueDotPath<orderShema>, string>> =
+          {};
+        for (const key in flatIssues.nested) {
+          const issueKey = key as v.IssueDotPath<orderShema>;
+          const nestedIssues = flatIssues.nested[issueKey];
+          if (nestedIssues?.length) {
+            formErrors[issueKey] = nestedIssues[0];
+          }
+        }
+        setFormError(formErrors as { customer: string; phone: string; address: string; });
+      } else if (error instanceof GenericError) {
+        console.error("GenericError caught:", error);
+      } else {
+        console.error("Unknown error:", error);
+      }
+    }
   };
 
   const reddirectToOrder = (orderId: string) => {
@@ -42,20 +71,26 @@ const Checkout = () => {
       <div className="form-inputs">
         <FormInput
           credentials="First Name"
-          error=""
           systemError=""
           type="text"
           name="first_name"
+          errorMessage={formError.customer}
         />
         <FormInput
           credentials="Phone number"
           type="text"
           name="phone_number"
+          errorMessage={formError.phone}
         ></FormInput>
-        <FormInput credentials="Adress" type="text" name="adress" />
+        <FormInput
+          credentials="Adress"
+          type="text"
+          name="adress"
+          errorMessage={formError.address}
+        />
       </div>
       <div className="checkbox-priority">
-        <Input type="checkbox" name="priority"></Input>
+        <Input type="checkbox" name="priority" variation=""></Input>
         <label>Want to yo give your order priority?</label>
       </div>
       <Btn type="submit">Order now from</Btn>
