daeddda2f2159c3c2c1ea80a2b7dddd2936fd3f0
Bernard Krehula <krehula.bernard456@gmail.com>
2026-05-26 21:35:33 +0200

    feat: add try catch block inside requestPreparingOrder function

--- src/api/requestPreparingOrder.ts
+++ src/api/requestPreparingOrder.ts
@@ -1,8 +1,17 @@
-import axios from "axios"
+import axios, { AxiosError } from "axios";
 
-export const requestPreparingOrder = async(orderId: string) => {
-    const response = await axios.get("https://react-fast-pizza-api.onrender.com/api/order/FB9SX4");
+export const requestPreparingOrder = async (orderId: string) => {
+  try {
+    const response = await axios.get(
+      `https://react-fast-pizza-api.onrender.com/api/order/${orderId}`,
+    );
     return response.data;
-}
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      console.error(error.response?.data.message);
+      throw new Error(error.response?.data.message);
+    }
+    throw error;
+  }
+};
 
-//Shvatiti kako requestati order koji sam zatrazio 23.5 u 3:01
